fe879d0339d447cbd6b389c0556ca3a5b7230782
wangxingixng <1129757968@qq.com>
2018-04-04 22:36:28 +0800

    添加react-native-swiper

--- App.js
+++ App.js
@@ -1,7 +1,36 @@
 import React from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text, StyleSheet } from 'react-native';
 import { StackNavigator } from 'react-navigation'; // Version can be specified in package.json
-
+import Swiper from 'react-native-swiper';
+var styles = StyleSheet.create({
+  wrapper: {
+    height:400,
+    width:400
+  },
+  slide1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#9DD6EB',
+  },
+  slide2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#97CAE5',
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#92BBD9',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
+  }
+})
 class HomeScreen extends React.Component {
   render() {
     return (
@@ -11,6 +40,17 @@ class HomeScreen extends React.Component {
           title="Go to Details"
           onPress={() => this.props.navigation.navigate('Details')}
         />
+        <Swiper style={styles.wrapper} showsButtons={true}>
+        <View style={styles.slide1}>
+          <Text style={styles.text}>Hello Swiper</Text>
+        </View>
+        <View style={styles.slide2}>
+          <Text style={styles.text}>Beautiful</Text>
+        </View>
+        <View style={styles.slide3}>
+          <Text style={styles.text}>And simple</Text>
+        </View>
+      </Swiper>
       </View>
     );
   }
